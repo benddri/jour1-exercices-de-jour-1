64b3a362b28433b8563af9bc12d735023077f650
Benddri <fatimabenddri@email.com>
2026-09-09 15:05:49 +0200

    jusqu'a ex 20

--- day01/exercices/exercice-15.js
+++ day01/exercices/exercice-15.js
@@ -17,4 +17,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let prixExtrait = "  349.99 MAD  ";
+
+let prix = Number(prixExtrait.trim().replace(" MAD", ""));
+
+console.log(prix === 349.99); 
+
+let total = prix * 2;
+console.log(total); 
